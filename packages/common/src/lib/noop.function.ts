3acca99e1c602c05c2e66928fd0c6b746b626d4c
Raphael Fleischlin <git@raeffs.dev>
2020-12-29 14:41:49 +0100

    add common package

---
+++ packages/common/src/lib/noop.function.ts
@@ -0,0 +1,7 @@
+/**
+ * A function that does nothing.
+ * @param _arguments A list of parameters that are not used.
+ */
+export function noop(..._arguments: unknown[]): void {
+  // nothing to do
+}
